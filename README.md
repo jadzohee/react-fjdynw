fbe8bbd3f234a4a926500751bf2a59d4013212ea
jadzohee <jadzohee@gmail.com>
2021-04-06 21:29:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fjdynw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fjdynw)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div class="title">
+      <h1>Les Fruits !</h1>
+    </div>
+  );
+}
